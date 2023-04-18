d39ff77d55565a9ef79d1b75420daaac18a3fd57
Draexx <p.carranza.is@gmail.com>
2023-04-18 11:26:30 -0400

    Agregado Patrulla  y atributos a clase campo de batalla con sus getter and setter

---
+++ src/clases/Barco/Patrulla.java
@@ -0,0 +1,8 @@
+package clases.Barco;
+
+public class Patrulla extends Barco{
+    public Patrulla(Integer tamanio, Boolean direccion) {
+        super(tamanio, direccion);
+    }
+
+}
--- src/clases/CampoBatalla/CampoBatalla.java
+++ src/clases/CampoBatalla/CampoBatalla.java
@@ -8,4 +8,59 @@ import java.util.ArrayList;
 public class CampoBatalla {
     private Coordenada disparo;
     private ArrayList<Barco> barcoEnCampo;
+    private Integer dimension;
+    private Coordenada coordenadas;
+    private ArrayList<Coordenada> disparosRealizados;
+
+    /**
+     * Constructor de la clase
+     */
+    public CampoBatalla() {
+        barcoEnCampo = new ArrayList<Barco>();
+        disparosRealizados = new ArrayList<Coordenada>();
+    }
+
+    /**
+     * Metodo que devuelve los disparos realizados
+     * @return Lista de disparos Realizados
+     */
+    public ArrayList<Coordenada> getDisparosRealizados() {
+        return disparosRealizados;
+    }
+
+    public void setDisparosRealizados(ArrayList<Coordenada> disparosRealizados) {
+        this.disparosRealizados = disparosRealizados;
+    }
+
+    public Coordenada getDisparo() {
+        return disparo;
+    }
+
+    public void setDisparo(Coordenada disparo) {
+        this.disparo = disparo;
+    }
+
+    public ArrayList<Barco> getBarcoEnCampo() {
+        return barcoEnCampo;
+    }
+
+    public void setBarcoEnCampo(ArrayList<Barco> barcoEnCampo) {
+        this.barcoEnCampo = barcoEnCampo;
+    }
+
+    public Integer getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Integer dimension) {
+        this.dimension = dimension;
+    }
+
+    public Coordenada getCoordenadas() {
+        return coordenadas;
+    }
+
+    public void setCoordenadas(Coordenada coordenadas) {
+        this.coordenadas = coordenadas;
+    }
 }
